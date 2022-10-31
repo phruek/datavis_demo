e44b78be58529d533ff3030a88e3d6d12c21e749
phruek <phruek.ko@gmail.com>
2022-10-31 22:17:50 +0700

    add file

--- pages/Page_phruek.py
+++ pages/Page_phruek.py
@@ -110,9 +110,9 @@ with tab2:
     # dfmap = gpd.read_file('https://bit.ly/thaigeojson')
     
     dfmap = pd.read_csv('thaigeo.csv')
-    # dfmap['geometry'] = gpd.GeoSeries.from_wkt(dfmap['geometry'])
-    # dfmap = gpd.GeoDataFrame(dfmap, geometry='geometry')
-    dfmap['geometry'] = dfmap.geometry.apply(lambda x: x.wkt).apply(lambda x: re.sub('"(.*)"', '\\1', x))
+    dfmap['geometry'] = gpd.GeoSeries.from_wkt(dfmap['geometry'])
+    dfmap = gpd.GeoDataFrame(dfmap, geometry='geometry')
+    # dfmap['geometry'] = dfmap.geometry.apply(lambda x: x.wkt).apply(lambda x: re.sub('"(.*)"', '\\1', x))
 
     dfmap['name'] = dfmap['name'].apply(lambda x: 'Nong Bua Lamphu' if x == 'Nong Bua Lam Phu' else x)
     dfplot = dfmap.merge(dfComplated, on='name', how='left', indicator=True)
